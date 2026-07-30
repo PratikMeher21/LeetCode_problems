5b9cfac7fa7decab2494bb199c80ee215a6780ec
Pratik Meher <pratikmeher2114@gmail.com>
2026-07-30 23:53:00 +0530

    Time: 0 ms (100%), Space: 19.4 MB (33.63%) - LeetHub

--- 0054-spiral-matrix/0054-spiral-matrix.py
+++ 0054-spiral-matrix/0054-spiral-matrix.py
@@ -15,17 +15,14 @@ class Solution:
             for i in range(top,bottom+1):
                 arr.append(matrix[i][right])
             right-=1
-
             if top<=bottom:
                 for i in range(right,left-1,-1):
                     arr.append(matrix[bottom][i])
                 bottom-=1
-
             if left <=right:
                 for i in range(bottom,top-1,-1):
                     arr.append(matrix[i][left])
                 left+=1
-                
         return arr
 
         
