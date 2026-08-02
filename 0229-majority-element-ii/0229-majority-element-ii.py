398f3a831c371904dd7d6a23bcb06867c86e110b
Pratik Meher <pratikmeher2114@gmail.com>
2026-08-02 23:25:50 +0530

    Time: 8 ms (48.71%), Space: 22.4 MB (76.24%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.py
+++ 0229-majority-element-ii/0229-majority-element-ii.py
@@ -1,15 +1,36 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> List[int]:
-        freq = {}
+        candidate1 = None
+        candidate2 = None
+
+        count1 = 0
+        count2 = 0
 
         for num in nums:
-            freq[num] = freq.get(num, 0) + 1
+
+            if num == candidate1:
+                count1 += 1
+
+            elif num == candidate2:
+                count2 += 1
+
+            elif count1 == 0:
+                candidate1 = num
+                count1 = 1
+
+            elif count2 == 0:
+                candidate2 = num
+                count2 = 1
+
+            else:
+                count1 -= 1
+                count2 -= 1
 
         result = []
 
-        for num, count in freq.items():
-            if count > len(nums) // 3:
-                result.append(num)
+        for candidate in [candidate1, candidate2]:
+            if candidate is not None and nums.count(candidate) > len(nums) // 3:
+                result.append(candidate)
 
         return result
         
